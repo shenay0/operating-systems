0c8df14ff46d06c076985687545ef1a326572a3b
shenay0 <shenay.orhan21@gmail.com>
2026-02-24 17:10:11 +0200

    Add 02-a-0500

---
+++ shell/02.commands/02-a-0500.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Move file
+
+#~
+mkdir -p practise-test/test1
+
+cd practise-test/test1
+#practise-test/test1
+touch test.txt
+
+cd
+#~
+mv practise-test/test1/test.txt practice-test
+
